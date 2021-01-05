aed0575aa1ced8725406681bc71723a1af00ebc5
DarthHamza <hamzamakia@gmail.com>
2021-01-05 11:58:08 +0300

    made some adjustments to the project content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className="text-center">
       <h2 className="section-heading text-uppercase">Hamza's Timeline</h2>
-      <h3 className="section-subheading text-muted">Stuff from Hamza's life</h3>
+      <h3 className="section-subheading text-muted">
+        Some stuff from Hamza's life
+      </h3>
     </div>
   );
 };
